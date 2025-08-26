71e6ff063e020af4434304133cc08fd8e02fd635
saiteja-konijeti <saitejateja457@gmail.com>
2025-08-26 11:31:55 +0530

    adding assets

---
+++ Assets/assests.js
@@ -0,0 +1,66 @@
+import b1 from './Birthdays/image(1).png';
+import b2 from './Birthdays/VC(10).png';
+import b3 from './Birthdays/b3.jpeg';
+import b4 from './Birthdays/b4.jpeg';
+import b5 from './Birthdays/b5.jpeg';
+import b6 from './Birthdays/b6.jpeg';
+import b7 from './Birthdays/b7.jpeg';
+import b8 from './Birthdays/b8.jpeg';
+import m1 from './Meternity/m1.png';
+import m2 from './Meternity/m2.png';
+import m3 from './Meternity/m3.png';
+import m4 from './Meternity/m4.png';
+import m5 from './Meternity/m5.jpeg';
+import n1 from './New born/n1.jpeg';
+import n2 from './New born/n2.jpeg';
+import n3 from './New born/n3.jpeg';
+import n4 from './New born/n4.jpeg';
+import n5 from './New born/n5.jpeg';
+import n6 from './New born/n6.jpeg';
+import p1 from './Prewedding/p1.jpeg';
+import p2 from './Prewedding/p2.jpeg';
+import p3 from './Prewedding/p3.jpeg';
+import p4 from './Prewedding/p4.jpeg';
+import p5 from './Prewedding/p5.jpeg';
+import p6 from './Prewedding/p6.jpeg';
+import p7 from './Prewedding/p7.jpeg';
+import p8 from './Prewedding/p8.jpeg';
+import p9 from './Prewedding/p9.jpeg';
+import p10 from './Prewedding/p10.jpeg';
+import p11 from './Prewedding/p11.jpeg';
+import p12 from './Prewedding/p12.jpeg';
+import p13 from './Prewedding/p13.jpeg';
+import p14 from './Prewedding/p14.jpeg';
+import p15 from './Prewedding/p15.jpeg';
+import p16 from './Prewedding/p16.jpeg';
+import p17 from './Prewedding/p17.jpeg';
+import w1 from './Wedd/w1.jpg';
+import w2 from './Wedd/w2.jpg';
+import w3 from './Wedd/w3.jpeg';
+import w4 from './Wedd/w4.jpeg';
+import w5 from './Wedd/w5.jpeg';
+import w6 from './Wedd/w6.jpeg';
+import w7 from './Wedd/w7.jpeg';
+import w8 from './Wedd/w8.jpeg';
+import w9 from './Wedd/w9.jpeg';
+import w10 from './Wedd/w10.jpeg';
+
+export const birthdayimages = {
+  b1,
+  b2,
+  b3,
+  b4,
+  b5, b6, b7, b8
+}
+export const merternityimages={
+  m1,m2,m3,m4,m5
+}
+export const newborn={
+  n1,n2,n3,n4,n5,n6
+}
+export const prewedd={
+  p1,p2,p3,p4,p5,p,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17
+}
+export const wedding={
+  w1,w2,w3,w4,w5,w6,w7,w8,w9,w10
+}
